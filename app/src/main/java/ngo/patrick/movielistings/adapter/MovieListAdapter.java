c5c18be01ac607c038f2619613dba30d34fc06c8
Patrick Ngo <patrick.ngo@hotmail.com>
2017-02-24 11:35:44 +0800

    added view holder pattern for MovieListAdapter
    renamed unclear var
    put hardcoded image base url in a const

--- app/src/main/java/ngo/patrick/movielistings/adapter/MovieListAdapter.java
+++ app/src/main/java/ngo/patrick/movielistings/adapter/MovieListAdapter.java
@@ -1,7 +1,6 @@
 package ngo.patrick.movielistings.adapter;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +13,7 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 import ngo.patrick.movielistings.R;
+import ngo.patrick.movielistings.api.TmdbAPI;
 import ngo.patrick.movielistings.model.PageListingResult.Result;
 
 /**
@@ -37,55 +37,31 @@ public class MovieListAdapter extends ArrayAdapter<Result>
     {
         View v = convertView;
 
-        if (v == null) {
             LayoutInflater vi;
             vi = LayoutInflater.from(getContext());
             v = vi.inflate(R.layout.list_item_movie_row, null);
         }
 
         //Get movie data from specified position
-        Result p = getItem(position);
 
-        //Find appropriate layout components and display respective data
-        if (p != null)
         {
-            TextView idTextView = (TextView) v.findViewById(R.id.id);
-            TextView captionTextView = (TextView) v.findViewById(R.id.title);
-            ImageView thumbnailView = (ImageView) v.findViewById(R.id.thumbnail);
-            TextView createdOnTextView = (TextView) v.findViewById(R.id.rating);
-            TextView updatedOnTextView = (TextView) v.findViewById(R.id.release_date);
-
-
             //title
-            if (captionTextView != null) {
-                captionTextView.setText(p.getOriginalTitle());
             }
 
             //popularity
-            if (createdOnTextView != null) {
-                createdOnTextView.setText(getContext().getString(R.string.rating) + p.getPopularity().toString());
             }
 
             //release date
-            if (updatedOnTextView != null) {
-                updatedOnTextView.setText(p.getReleaseDate());
             }
 
             //thumbnail
-            if (thumbnailView != null)
             {
-                if (p.getPosterPath() != null)
                 {
-                    String baseUrl = "https://image.tmdb.org/t/p/w185";
-                    Picasso.with(getContext()).load(baseUrl + p.getPosterPath()).into(thumbnailView);
                 }
             }
 
             //id (FOR DEBUG)
-            if (idTextView != null)
             {
-                idTextView.setText(Integer.toString(p.getId()));
-
             }
         }
 
